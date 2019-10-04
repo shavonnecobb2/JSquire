7debf27640a0f4d81402487aa5e445159cb670e6
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-10-04 15:28:11 -0500

    new Streams kihon with the third test not working

--- src/main/java/kihons/StreamKihon.java
+++ src/main/java/kihons/StreamKihon.java
@@ -4,38 +4,40 @@ import framework.Item;
 import framework.bases.StreamsKihonBase;
 import framework.exceptions.NotImplementedYetException;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamKihon extends StreamsKihonBase {
 
     @Override
     protected List<String> getTheNamesOfAllItems(List<Item> items) {
-        throw new NotImplementedYetException();
+        return items.stream().map(item -> item.getName()).collect(Collectors.toList());
     }
 
     @Override
     protected List<Item> sortItemsAlphabeticallyBasedOnName(List<Item> items) {
-        throw new NotImplementedYetException();
+        return items.stream().sorted(Comparator.comparing(Item::getName)).collect(Collectors.toList());
     }
 
     @Override
     protected List<Item> sortOnCostFromLestToMostExpensiveAllItemsThatCostLessThanOrEqualToMyMonies(List<Item> items, int monies) {
-        throw new NotImplementedYetException();
+        return items.stream().filter(item -> item.getCost() <= monies).sorted(Comparator.comparing(Item::getCost)).collect(Collectors.toList());
     }
 
     @Override
     protected Item getTheItemWithTheMostCost(List<Item> items) {
-        throw new NotImplementedYetException();
+        return items.stream().max(Comparator.comparing(Item::getCost)).orElseThrow();
     }
 
     @Override
     protected int getTheAverageCostRoundedDown(List<Item> items) {
-        throw new NotImplementedYetException();
+        return items.stream().mapToInt(x -> x.getCost()).sum() / items.size();
     }
 
     @Override
     protected void addAllItemsToMyMapWIthKetNameAndValueOfCost(List<Item> items, Map<String, Integer> myMap) {
-        throw new NotImplementedYetException();
+        items.stream().forEach(e -> myMap.put(e.getName(), e.getCost()));
     }
 }
