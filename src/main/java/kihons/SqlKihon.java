ed13d409bc3dc3ae62739b77a9e35b513ed93bd2
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-10-04 09:02:08 -0500

    morning drills

--- src/main/java/kihons/SqlKihon.java
+++ src/main/java/kihons/SqlKihon.java
@@ -29,31 +29,31 @@ public class SqlKihon extends SqlKihonBase {
 
     @Override
     protected String Select_All_Fields_And_Rows_From_Person() {
-        throw new NotImplementedYetException();
+        return "SELECT * FROM Person";
     }
 
     @Override
     protected String selectAllFieldsFromPersonJoinedToAddress() {
-        throw new NotImplementedYetException();
+        return "SELECT * FROM Person JOIN Address ON Person.PersonId = Address.PersonId";
     }
 
     @Override
     protected String selectFirstNameFromPersonWhereLastNameEqualsRayburn() {
-        throw new NotImplementedYetException();
+        return "SELECT firstName FROM Person WHERE lastName = 'Rayburn'";
     }
 
     @Override
     protected String selectAllFieldsFromPersonLeftOuterJoinedToAddress() {
-        throw new NotImplementedYetException();
+        return "SELECT * FROM Person LEFT JOIN Address ON Person.PersonId = Address.PersonId";
     }
 
     @Override
     protected String insertPersonId4NamedMikeJohnsonAge5ToPerson() {
-        throw new NotImplementedYetException();
+        return "INSERT INTO Person (PersonId, FirstName, LastName, Age) VALUES (4,'Mike', 'Johnson', 5)";
     }
 
     @Override
     protected String updateAllLastNamesRayburnToJohnsonInPerson() {
-        throw new NotImplementedYetException();
+        return "UPDATE Person Set lastName = 'Johnson' WHERE lastName = 'Rayburn'";
     }
 }
